5981756e51c8f0fc09a66a9d6f14fd8801e50bba
Tyler Tadej <tylertadej@users.noreply.github.com>
2014-07-10 23:35:19 -0700

    20 minute mark of tutorial video

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,7 @@
   angular.module('readingList', [ ])
     .controller('ReadingListCtrl', function(){
       this.books = books;
+      this.genres = genres;
     })
   ;  
 
@@ -29,28 +30,28 @@
   var books = [
     {
       title: 'A Game of Thrones: A Song of Ice and Fire',
-      author: 'George R.R. Martin',
+      authors: ['George R.R. Martin', 'DOS Computer'],
       isbn: '0553593714',
       review: 'The most inventive and entertaining fantasy saga of our time—warrants one hell of an introduction. I loved this book!',
       rating: 4,
       genres: { 'non-fiction': true, fantasy: true }
     },{
       title: 'HTML for Babies',
-      author: 'John C Vanden-Heuvel Sr',
+      authors: ['John C Vanden-Heuvel Sr'],
       isbn: '0615487661',
       review: "It's never too early to be standards compliant! I taught my little one mark-up in under one hour!",
       rating: 5,
       genres: { fiction: true }
     },{
       title: 'A is for Array',
-      author: 'Brandon J Hansen',
+      authors: ['Brandon J Hansen', 'John Fake Doe'],
       isbn: '1489522212',
       review: 'A is for Array is the ABC book for future programmers. Filled with fun illustrations and simple real-world examples, my children loved seeing my world intertwined with theirs!',
       rating: 4,
       genres: { fiction: true }
     },{
       title: 'The Dragon Reborn',
-      author: 'Robert Jordan',
+      authors: ['Robert Jordan'],
       isbn: '0812513711',
       review: 'The Wheel weaves as the Wheel wills, and we are only the thread of the Pattern. Moiraine',
       rating: 4,
