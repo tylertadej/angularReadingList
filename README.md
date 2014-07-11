4285a008eff39ebe838fdc4eadc59ae4e9583753
Tyler Tadej <tylertadej@users.noreply.github.com>
2014-07-10 23:12:24 -0700

    app files connected and ready

--- README.md
+++ README.md
@@ -7,8 +7,19 @@ from Code School:
 
 After playing the Shaping Up With Angular.js course, you can watch this in-depth screencast hosted by Gregg Pollack and Alyssa Nicoll to learn the practical steps of building an Angular.js app from scratch.
 
+Video:
 https://www.codeschool.com/code_tv/soup-to-bits-shaping-up-with-angular-js
 
+Code School Repos:
+
+Empty
+https://github.com/codeschool/AngularReadinglist
+
+Completed
+https://github.com/codeschool/AngularJSSTB
+
+
+>>>
 
 
 Code School is great for beginners and intermediate coders, check 'em out !
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * @ngdoc overview
  * @name readingListApp
@@ -8,26 +6,57 @@
  *
  * Main module of the application.
  */
-angular
-  .module('readingList', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+
+
+
+(function(){
+
+  'use strict';
+
+  angular.module('readingList', [ ])
+    .controller('ReadingListCtrl', function(){
+      this.books = books;
+    })
+  ;  
+
+
+
+
+
+
+  var genres = [ 'fable', 'fantasy', 'fiction', 'folklore', 'horror', 'humor', 'legend', 'metafiction', 'mystery', 'mythology', 'non-fiction', 'poetry' ];
+
+  var books = [
+    {
+      title: 'A Game of Thrones: A Song of Ice and Fire',
+      author: 'George R.R. Martin',
+      isbn: '0553593714',
+      review: 'The most inventive and entertaining fantasy saga of our time—warrants one hell of an introduction. I loved this book!',
+      rating: 4,
+      genres: { 'non-fiction': true, fantasy: true }
+    },{
+      title: 'HTML for Babies',
+      author: 'John C Vanden-Heuvel Sr',
+      isbn: '0615487661',
+      review: "It's never too early to be standards compliant! I taught my little one mark-up in under one hour!",
+      rating: 5,
+      genres: { fiction: true }
+    },{
+      title: 'A is for Array',
+      author: 'Brandon J Hansen',
+      isbn: '1489522212',
+      review: 'A is for Array is the ABC book for future programmers. Filled with fun illustrations and simple real-world examples, my children loved seeing my world intertwined with theirs!',
+      rating: 4,
+      genres: { fiction: true }
+    },{
+      title: 'The Dragon Reborn',
+      author: 'Robert Jordan',
+      isbn: '0812513711',
+      review: 'The Wheel weaves as the Wheel wills, and we are only the thread of the Pattern. Moiraine',
+      rating: 4,
+      genres: { 'non-fiction': true, fantasy: true }
+    }
+  ];
+
+
+})();
